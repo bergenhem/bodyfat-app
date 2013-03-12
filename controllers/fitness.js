f7c8450474cf3a9101f4373efba668cb6a8497cb
Carl Bergenhem <cbergenhem@gmail.com>
2013-03-11 20:36:36 -0400

    Add logic to not add bodyfat duplicates in the database

--- controllers/fitness.js
+++ controllers/fitness.js
@@ -1,33 +1,58 @@
 var BodyFat = require('../models/bodyfat');
+var moment = require('moment');
 
 exports.addBodyFat = function(req, res) {
 	var itemToInsert = req.body;
 
 	var createdBodyFat = new BodyFat();
 
-	if(itemToInsert.date) createdBodyFat.date = moment(itemToInsert.date).format('YYYY-MM-DD');
-	if(itemToInsert.gender) createdBodyFat.gender = itemToInsert.gender;
-	if(itemToInsert.age) createdBodyFat.age = itemToInsert.age;
-	if(itemToInsert.unit) createdBodyFat.unit = itemToInsert.unit;
-	if(itemToInsert.weight) createdBodyFat.weight = itemToInsert.chest;
-	if(itemToInsert.chest) createdBodyFat.chest = itemToInsert.chest;
-	if(itemToInsert.thigh) createdBodyFat.thigh = itemToInsert.thigh;
-	if(itemToInsert.abs) createdBodyFat.abs = itemToInsert.abs;
+	if(itemToInsert.date) {
+		var formatString = moment(itemToInsert.date).format('YYYY-MM-DD');
+		BodyFat.findOne({ 'date': formatString }, 'date', function(err, bodyFat) {
+			if(err) {
+				console.log('Error in query:\n' + err);
+			}
+			else {
+				//Did not find
+				if(!bodyFat) {
+					createdBodyFat.date = formatString;
+					if(itemToInsert.gender) createdBodyFat.gender = itemToInsert.gender;
+					if(itemToInsert.age) createdBodyFat.age = itemToInsert.age;
+					if(itemToInsert.unit) createdBodyFat.unit = itemToInsert.unit;
+					if(itemToInsert.weight) createdBodyFat.weight = itemToInsert.chest;
+					if(itemToInsert.chest) createdBodyFat.chest = itemToInsert.chest;
+					if(itemToInsert.thigh) createdBodyFat.thigh = itemToInsert.thigh;
+					if(itemToInsert.abs) createdBodyFat.abs = itemToInsert.abs;
 
-	createdBodyFat.calcBodyFat();
+					createdBodyFat.calcBodyFat();
 
-	createdBodyFat.save(function(err, bodyFat) {
-		if(err) {
-			console.log("Error when saving:\n" + err);
-			res.writeHead(500, "Internal Server Error", {'content-type': 'application/json'});
-			res.end();
-		}
-		else {
-			res.writeHead(201, "Created", {'content-type': 'application/json'});
-			res.write(JSON.stringify(createdBodyFat));
-			res.end();
-		}
-	});
+					createdBodyFat.save(function(err, bodyFat) {
+						if(err) {
+							console.log("Error when saving:\n" + err);
+							res.writeHead(500, "Internal Server Error", {'content-type': 'application/json'});
+							res.end();
+						}
+						else {
+							res.writeHead(201, "Created", {'content-type': 'application/json'});
+							res.write(JSON.stringify(createdBodyFat));
+							res.end();
+						}
+					});
+				}
+				//Item found, return 409 to indicate it already exists
+				else {
+					res.writeHead(409, "Conflict", {'content-type': 'application/json'});
+					res.end();
+				}
+			}
+		});
+	}
+
+	//Invalid request - no date provided
+	else {
+		res.writeHead(500, "Internal Server Error", {'content-type': 'application/json'});
+		res.end();
+	}
 }
 
 exports.getAllBodyFat = function(req, res) {
