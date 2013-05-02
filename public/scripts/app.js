973bf53278b46509da543d3836207102afb9e37b
bergenhem <cbergenhem@gmail.com>
2013-05-01 20:54:32 -0400

    Rename theme to 'Fitness'

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -3,7 +3,15 @@ window.FitnessApp = (function($){
 	var _fitnessApp = {};
 
 	_fitnessApp.init = function() {
-		$('#caliperWindow').kendoWindow();
+		var modalWindow = $('#caliperWindow');
+		modalWindow.kendoWindow({
+			title: "Before We Begin",
+			modal: true,
+			draggable: false,
+			resizable: false,
+			width: 400,
+			height: 250
+		}).data('kendoWindow').center();;
 	}
 
 	return _fitnessApp;
