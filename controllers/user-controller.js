9be7c1371c25ca422711ee10a221decbd9f5a7be
bergenhem <cbergenhem@gmail.com>
2014-12-25 22:33:05 -0500

    Update all user controller responses

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -15,8 +15,9 @@ exports.addUser = function(req, res) {
 
 		UserModel.findOne({ 'userName': USER_NAME }, 'userName', function(err, users) {
 			if(err) {
-				console.log('Error in finding unique user name:\n' + err);
+				console.log('Error in finding user by name:\n' + err);
 				res.writeHead(500, 'Internal Server Error', { 'content-type': 'application/json' });
+				res.write(JSON.stringify({ message: 'Error in finding user by name.' }));
 				res.end();
 			}
 			else {
@@ -24,7 +25,10 @@ exports.addUser = function(req, res) {
 
 					encryption.genSalt(10, function(err, salt) {
 						if(err) {
-							console.log('Error when generating salt\n');
+							console.log('Error in generating salt:\n' + err);
+							res.writeHead(500, 'Internal Server Error', { 'content-type': 'application/json' });
+							res.write(JSON.stringify({ message: 'Error in generating salt' }));
+							res.end();
 						}
 						else {
 							encryption.hash(userPass, salt, function(err, hash) {
@@ -41,6 +45,7 @@ exports.addUser = function(req, res) {
 									if(err) {
 										console.log('Error when saving user:\n' + err);
 										res.writeHead(500, 'Internal Server Error', {'content-type': 'application/json'});
+										res.write(JSON.stringify({ message: 'Error when saving user.' }));
 										res.end();
 									}
 									else {
@@ -55,6 +60,7 @@ exports.addUser = function(req, res) {
 				}
 				else { //username already exists
 					res.writeHead(409, 'Conflict', {'content-type': 'application/json'});
+					res.write(JSON.stringify({ message: 'Username already exists.' }));
 					res.end();
 				}
 			}
@@ -69,8 +75,9 @@ exports.saveSettings = function(req, res) {
 
 	UserModel.findOne({ 'userName': currentUser }, function(err, foundUser) {
 		if(err) {
-			console.log('Errr in finding user to save settings to:\n' + err);
+			console.log('Error in finding user to save settings to:\n' + err);
 			res.writeHead(500, 'Internal Server Error', { 'content-type': 'application/json' });
+			res.write(JSON.stringify({ message: 'Error when finding user to save settings to.' }));
 			res.end();
 		}
 		else {
@@ -84,18 +91,22 @@ exports.saveSettings = function(req, res) {
 				foundUser.save(function(err, user) {
 					if(err) {
 						console.log('Error when saving user settings:\n' + err);
-						res.writeHead(500, 'Internal Server Error', {'content-type': 'application/json'});
+						res.writeHead(500, 'Internal Server Error', {'content-type': 'application/json' });
+						res.write(JSON.stringify({ message: 'Error when saving user settings.' }));
 						res.end();
 					}
 					else {
-						res.writeHead(200, 'OK', {'content-type': 'application/json'});
-						res.end(JSON.stringify({ message: 'Settings saved successfully' }));
+						console.log('Successfully saving user settings for:\n' + currentUser);
+						res.writeHead(200, 'OK', { 'content-type': 'application/json' });
+						res.write(JSON.stringify({ message: 'Settings saved successfully' }));
+						res.end();
 					}
 				});
 			}
 			else {
 				console.log('Did not find a user when saving settings');
 				res.writeHead(500, 'Internal Server Error', { 'content-type': 'application/json' });
+				res.write(JSON.stringify({ message: 'Unable to find user when saving settings' }));
 				res.end();
 			}
 		}
@@ -111,6 +122,7 @@ exports.loadSettings = function(req, res) {
 		if(err) {
 			console.log('Error in finding user when loading settings:\n' + err);
 			res.writeHead(500, 'Internal Server Error', { 'content-type': 'application/json' });
+			res.write(JSON.stringify({ message: 'Error in finding user when loading settings.' }));
 			res.end();
 		}
 		else {
@@ -129,6 +141,7 @@ exports.loadSettings = function(req, res) {
 			else {
 				console.log('Did not find a user when loading settings');
 				res.writeHead(500, 'Internal Server Error', { 'content-type': 'application/json' });
+				res.write(JSON.stringify({ message: 'No user exists with that user name.' }));
 				res.end();
 			}
 		}
